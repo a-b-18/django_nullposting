3d6ecb3e980886305a557b73c98741c586dfa3d3
AZBZ34 <alexbeazley18@outlook.com>
2021-08-01 08:39:10 +1000

    part 7 complete.

--- django_nullposting/settings.py
+++ django_nullposting/settings.py
@@ -126,4 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'forum-home'
+LOGIN_URL = 'login'
