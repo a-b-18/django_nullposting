49c1b6f1e76288d147062948a77b4127eb9276dc
AZBZ34 <alexbeazley18@outlook.com>
2021-07-31 19:03:12 +1000

    part 6 complete.

--- django_nullposting/urls.py
+++ django_nullposting/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('', include('forum.urls')),
     path('admin/', admin.site.urls),
     path('forum/', include('forum.urls')),
+    path('register/', user_views.register, name='register'),
 ]
