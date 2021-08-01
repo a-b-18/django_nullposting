5a619ab45fa99ffe231faa29bc33240a61baf411
AZBZ34 <alexbeazley18@outlook.com>
2021-08-01 14:55:42 +1000

    part 10 complete.

--- forum/urls.py
+++ forum/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostUpdateView, 
+    PostCreateView,
+    PostDeleteView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='forum-home'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='about-home'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('', PostListView.as_view(), name='forum-home'),
 ]
