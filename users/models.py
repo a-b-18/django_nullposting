bbaeb4455af42f4050ec6cc4763187c76eb61325
AZBZ34 <alexbeazley18@outlook.com>
2021-08-02 11:56:47 +1000

    default profile pic fixed.

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='profile_pics\\default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default_profile.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
